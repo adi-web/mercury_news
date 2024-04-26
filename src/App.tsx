7c33a487dffd5ceb84914bfc5f1cdaa258c86d22
admir <mucaj00@gmail.com>
2024-04-26 03:23:04 +0200

    aggiunto nav e separato header in 3 div

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import Body from "./components/Body";
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="root-page">
+      <Header />
+        <div id="bho">
+        <Body />
+        </div>
 
     </div>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,85 @@
+import React, {useState} from 'react'
+import "./Header.css"
+
+function Header() {
+
+    const [adi,setClick]=useState(false)// qua ho creato un stato click e la funzione setClick che mi permette di
+
+
+    const handleClick= () =>{
+        const appRoot=document.getElementById('bho');
+        // @ts-ignore
+        appRoot.style.opacity=0.5;
+
+        setClick(!adi)}
+
+
+    return (
+        <header className="header">
+
+        <div className="nav_wrapper_primary">
+            <div className="nav_wrapper_secondary">
+                <div className="first_nav">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col ">
+                                <div>
+                                <div className="icon_nav" onClick={handleClick}>
+                                    <i className={adi ? 'fas fa-times' : 'fas fa-bars'} aria-hidden="true"></i>
+                                </div>
+
+                                   <div className={adi ? 'nav-menu active root': 'nav-menu'} >
+                                    <ul className="nav">
+                                        <li>
+                                            cisdi
+                                        </li>
+                                    </ul>
+
+                                   </div>
+                                </div>
+
+                            </div>
+                            <div className="col-6">
+                                <div className={adi ? 'active-title': 'title-page'}>
+                                    <h2 className="title"> The Mercury News</h2>
+                                </div>
+                            </div>
+                            <div className=" col">
+                                <div className="container-button">
+                                    <button type="button" className="btn btn-primary">Primary</button>
+                                    <button type="button" className="btn btn-secondary">Secondary</button>
+                                    <div className="icon-search">
+                                        <i className="fa fa-search" aria-hidden="true"></i>
+                                    </div>
+                                </div>
+
+                                <div className="container-icon-button">
+
+                                    <div className="icon-user">
+                                        <i className="fa fa-user" aria-hidden="true"></i>
+                                    </div>
+                                    <div className="icon-search">
+                                        <i className="fa fa-search" aria-hidden="true"></i>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+                <div className="middle-nav">
+
+
+                </div>
+
+            </div>
+        </div>
+        </header>
+    )
+}
+
+export default Header
