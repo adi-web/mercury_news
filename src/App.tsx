e0baaa2357f3db953923b4b05cb0b1271173ce62
admir <mucaj00@gmail.com>
2024-05-20 20:04:47 +0200

    added box-shadow fooote

--- src/App.tsx
+++ src/App.tsx
@@ -8,39 +8,51 @@ import {useEffect} from "react";
 
 function App() {
 
-const [alert,setAlert]=useState(true);
-const handleAlert=()=>{
+    const [alert, setAlert] = useState(true);
+    const handleAlert = () => {
 
-    setAlert(!alert);
-}
-
-
-
-  return (
-
-      <div className={alert ? 'center-alert ':''}>
-
-          <div className={alert ? 'disable':'header-container'}>
-              <Header/>
-          </div>
-            <div id="main-container" className={alert ? 'disable': 'container-xxl' }>
-            <Main />
-            </div>
-          <div  className={alert ? 'disable': '' }>
-          <Footer />
-          </div>
-
-          <div className={alert ? 'container-alert':'disable'}>
-              <h3>
-                  This is a replica of the Mercury News
-              </h3>
-                <button className="btn-alert" onClick={handleAlert}>Show the Site</button>
-
-          </div>
+        setAlert(!alert);
+    }
 
 
-      </div>
-  );
+    return (
+
+<>
+    {/* header  */}
+        <div className={alert ? 'disable' : 'header-container'}>
+            <Header/>
+        </div>
+
+
+    {/* main page */}
+    <div id="main-container" className={alert ? 'disable' : 'container-xxl'}>
+        <Main/>
+    </div>
+
+
+    {/* Footer  */}
+    <div className={alert ? 'disable' : 'footer'}>
+        <Footer/>
+    </div>
+
+
+
+    {/* alert of replica mercury news */}
+
+    <div className={alert ? 'center-alert ' : 'alert-disable '}>
+        <div className={alert ? 'container-alert' : 'disable'}>
+            <h3>
+                This is a replica of the Mercury News
+            </h3>
+            <button className="btn-alert" onClick={handleAlert}>Show the Site</button>
+
+        </div>
+
+
+    </div>
+</>
+)
+    ;
 }
 
 export default App;
